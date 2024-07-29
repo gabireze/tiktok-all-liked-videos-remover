ff2990713b8b7beb477a5e6fde901977e1b40668
Gabriel de Rezende Gonçalves <gabireze@gmail.com>
2024-07-29 17:33:25 -0300

    Update manifest.json version to 1.1.1 and remove unused script.js file

--- script.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  document.getElementById("startButton").addEventListener("click", function () {
-    chrome.runtime.sendMessage({ action: "removeLikedVideos" });
-  });
-});
